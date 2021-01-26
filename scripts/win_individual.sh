fc52cc99414daea078037b49ee2cc3ee27942fac
NakajimaTatusya <tatsuya.nakajima@jp.ricoh.com>
2021-01-26 16:43:39 +0900

    tailコマンドのヒントをエコー

--- scripts/win_individual.sh
+++ scripts/win_individual.sh
@@ -3,5 +3,6 @@
 cd ~/challenges_ansible
 echo "個別設定を実行"
 timestamp=`date "+%Y%m%d%H%M%S"`
+echo "状況確認コマンド$> tail -f /home/tnakajima/challenges_ansible/logs/${timestamp}-win_individual_setup.log  を実行"
 nohup ansible-playbook -vvv win_individual_setup.yml > logs/${timestamp}-win_individual_setup.log &
 exit 0
--- scripts/win_update.sh
+++ scripts/win_update.sh
@@ -3,5 +3,6 @@
 cd ~/challenges_ansible
 echo "Windows Updateを実行"
 timestamp=`date "+%Y%m%d%H%M%S"`
+echo "状況確認コマンド$> tail -f /home/tnakajima/challenges_ansible/logs/${timestamp}-win_update.log  を実行"
 nohup ansible-playbook -vvv win_update.yml > logs/${timestamp}-win_update.log &
 exit 0
