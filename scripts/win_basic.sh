62a949034d58786a60055de088ff565fc209eec8
NakajimaTatusya <tatsuya.nakajima@jp.ricoh.com>
2021-01-26 17:37:45 +0900

    状況確認メッセージ変更

--- scripts/win_basic.sh
+++ scripts/win_basic.sh
@@ -3,6 +3,6 @@
 cd ~/challenges_ansible
 echo "基本設定を実行"
 timestamp=`date "+%Y%m%d%H%M%S"`
-echo "状況確認コマンド$> tail -f /home/tnakajima/challenges_ansible/logs/${timestamp}-win_basic_setup.log  を実行"
+echo "状況確認を行うには> tail -f /home/tnakajima/challenges_ansible/logs/${timestamp}-win_basic_setup.log  を実行してください"
 nohup ansible-playbook -vvv win_basic_setup.yml > logs/${timestamp}-win_basic_setup.log &
 exit 0
